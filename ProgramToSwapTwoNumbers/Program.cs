6e7fbdc1af18d104dead17e91a984b8b916b96fb
Sachin Gurung <saching061@hotmail.com>
2019-12-10 21:24:53 -0800

    Add comments

--- ProgramToSwapTwoNumbers/Program.cs
+++ ProgramToSwapTwoNumbers/Program.cs
@@ -10,6 +10,34 @@ namespace ProgramToSwapTwoNumbers
     {
         static void Main(string[] args)
         {
+            /*
+            C# Program to Swap two Numbers
+            */
+            {
+                int num1, num2, temp;
+                //Input first number
+                Console.Write("\nEnter the First Number : ");
+                num1 = int.Parse(Console.ReadLine());
+                //Input second number
+                Console.Write("\nEnter the Second Number : ");
+                num2 = int.Parse(Console.ReadLine());
+                temp = num1;
+                num1 = num2;
+                num2 = temp;
+                Console.Write("\nAfter Swapping : ");
+                Console.Write("\nFirst Number : " + num1);
+                Console.Write("\nSecond Number : " + num2);
+                Console.Read();
+            }
         }
     }
 }
+ /*
+ Result:
+ Enter the First Number : 23
+Enter the Second Number : 25
+After Swapping :
+First Number : 25
+Second Number : 23
+ */
+
